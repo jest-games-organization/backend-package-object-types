89d2d021af7460ff7b28229a400444170cabf00c
rconrardy <conrardy@umich.edu>
2022-10-27 19:30:27 -0400

    Add DataObject and Enumerable types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+export type { DataObject } from './DataObject';
+export type { Enumerable } from './Enumerable';
 export type { Maybe } from './Maybe';
 export type { Nullable } from './Nullable';
 export type { Optional } from './Optional';
